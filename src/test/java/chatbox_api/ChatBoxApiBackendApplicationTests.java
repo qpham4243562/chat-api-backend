f9a2faf422f280bb90576675ea131411d6287b2c
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-09-25 14:08:32 +0700

    feat: push main

---
+++ src/test/java/chatbox_api/ChatBoxApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package chatbox_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBoxApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
