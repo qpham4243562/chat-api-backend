aba61d1e3c62f19adf90f323cd959f0b5e3f1dcb
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-09-26 19:40:44 +0700

    feat: Thêm lớp ApiResponse và chuẩn hóa phản hồi API

--- src/main/java/chatbox_api/model/User.java
+++ src/main/java/chatbox_api/model/User.java
@@ -1,5 +1,8 @@
 package chatbox_api.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,12 +11,21 @@ public class User {
 
     @Id
     private String id;
+
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+
     private String role;
     private boolean active;
     private String activationCode;
-    private String email;
     private String resetToken;
 
     public User() {}
