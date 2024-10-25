592a810ad0dd7102418cd4d16c9f2a7d6977594d
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-10-25 20:23:46 +0700

    Update WebSocketConfig.java

--- src/main/java/chatbox_api/config/WebSocketConfig.java
+++ src/main/java/chatbox_api/config/WebSocketConfig.java
@@ -1,8 +1,10 @@
 package chatbox_api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,9 +24,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue")
-                .setTaskScheduler(new DefaultManagedTaskScheduler())
-                .setHeartbeatValue(new long[] {15000, 15000});
+                .setTaskScheduler(heartbeatTaskScheduler())  // Cấu hình TaskScheduler
+                .setHeartbeatValue(new long[] {15000, 15000});  // 15 giây heartbeat
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler heartbeatTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);  // Bạn có thể điều chỉnh kích thước pool nếu cần
+        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        taskScheduler.initialize();
+        return taskScheduler;
     }
 }
