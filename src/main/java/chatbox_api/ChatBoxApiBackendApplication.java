84553683a88bdc73de73c0a9d8dea87aebc0680e
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-10-26 11:44:36 +0700

    feat: thêm cron job

--- src/main/java/chatbox_api/ChatBoxApiBackendApplication.java
+++ src/main/java/chatbox_api/ChatBoxApiBackendApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@EnableScheduling
 @SpringBootApplication
+@EnableScheduling
 public class ChatBoxApiBackendApplication {
 
 	public static void main(String[] args) {
