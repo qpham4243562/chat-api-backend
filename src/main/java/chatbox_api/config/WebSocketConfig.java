95642045d1340126a3562615a4ccd8f5e13515bb
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-10-26 10:35:30 +0700

    Update WebSocketConfig.java

--- src/main/java/chatbox_api/config/WebSocketConfig.java
+++ src/main/java/chatbox_api/config/WebSocketConfig.java
@@ -23,19 +23,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic", "/queue")
-                .setTaskScheduler(heartbeatTaskScheduler())  // Cấu hình TaskScheduler
-                .setHeartbeatValue(new long[] {15000, 15000});  // 15 giây heartbeat
+        registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
-    }
-
-    @Bean
-    public ThreadPoolTaskScheduler heartbeatTaskScheduler() {
-        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(1);  // Bạn có thể điều chỉnh kích thước pool nếu cần
-        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
-        taskScheduler.initialize();
-        return taskScheduler;
     }
 }
